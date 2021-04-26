144e5626e6bca98923c63d16f91a602b1262f60f
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-04-26 19:19:50 -0300

    Update js-video-aleatorio.js

--- js-video-aleatorio.js
+++ js-video-aleatorio.js
@@ -1,5 +1,15 @@
 // basta colar no console do navegador
 
-let video = document.querySelectorAll('a#video-title')
-let sorteado = Math.round(Math.random() * video.length)
-window.location.href = video[sorteado]
+let searchQuery = (/^(http|https):\/\/www.youtube.com\/results\?search_query=(.*)|www.youtube.com\/results\?search_query=(.*)/g).test(window.location.href)
+
+if(searchQuery){
+    randomVideo('a#video-title')
+}else{
+    randomVideo('#video-title-link')
+}
+
+function randomVideo(element) {
+    let video = document.querySelectorAll(element)
+    let sorteado = Math.round(Math.random() * video.length)
+    window.location.href = video[sorteado]
+}
